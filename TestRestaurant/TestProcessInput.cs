b54367f74aef1068c7a92ebb5cf66dbc1d0a41b3
Sebastian Majko <smajko31@gmail.com>
2018-04-20 23:43:21 -0400

    add unit test

---
+++ TestRestaurant/TestProcessInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRestaurant
+{
+    [TestClass]
+    public class TestProcessInput
+    {
+        [TestMethod]
+        public void GetRestaurants()
+        {
+        }
+
+        public void GetReviews()
+        {
+        }
+    }
+}
---
+++ TestRestaurant/TestValidateInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRestaurant
+{
+    [TestClass]
+    public class TestValidateInput
+    {
+        [TestMethod]
+        public void ValidateCommand()
+        {
+        }
+    }
+}
